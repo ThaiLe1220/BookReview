3f13a008223e8416765a2bf2278fdf6d5d671ace
Thai Le <lehongthai2000@gmail.com>
2023-07-28 04:14:54 +0700

    Initial Commit

---
+++ PizzaApp/PizzaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaAppApp.swift
+//  PizzaApp
+//
+//  Created by Lê Ngọc Trâm on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PizzaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
